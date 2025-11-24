857be15edc670ca172b196e2ae9f2d48b9e03ad7
JB-usf <JoelBritto@usf.edu>
2025-11-23 21:33:17 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ score_change = second_score - first_score
 hour_to_score_ratio = score_change / total_hours
 print("Average daily study hours:", daily_hours)
 print("Score change per hour studied:", hour_to_score_ratio)
+try:
+    total_hours= float(total_hours)
+    first_score = float(first_score)
+    second_score = float(second_score)
+except ValueError:
+    print("Please enter a valid number and valid test scores.")
+    exit()
