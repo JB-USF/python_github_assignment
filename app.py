9e29f68e857acbc40932be59bee3fcb03061cbeb
JB-usf <JoelBritto@usf.edu>
2025-11-23 21:27:46 -0500

    Add more user input prompt

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 total_hours = input("Enter total hours studied this week: ")
+first_score = input("Enter first test score: ")
+second_score = input("Enter second test score: ")
 total_hours = float(total_hours)
 daily_hours = total_hours / 7
